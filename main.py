97d5f172bcc1314324e1316490ad55b6878d4426
GeonwooVincentKim <kdsnop@gmail.com>
2021-08-26 19:39:25 +0900

    [Feature] - Added friendships tuple

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ for i in users:
 for user in users:
     user["friends"] = []
     print(user["friends"])
+
+
+friendships = [
+    (0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+    (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)
+]
+
+for i in friendships:
+    print("me: {0}".format(i))
