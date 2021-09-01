090ba6486de4f51c598dba2fa5100db54b9e37c6
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-01 10:50:59 +0900

    [Feature] - Updating program..

--- main.py
+++ main.py
@@ -1,71 +1,10 @@
-users = [
-    {"id": 0, "name": "Hero"},
-    {"id": 1, "name": "David"}
-]
+import sys
+from pathlib import Path
+sys.path[0] = str(Path(sys.path[0]).parent)
+# sys.path[1] = str(Path(sys.path[1]).parent)
 
-for i in users:
-    print("id : {0}".format(i["id"]))
-    print("name : " + i["name"])
-    print("\n")
-
-# for user in users:
-    # user["friends"] = []
-    # print(user["friends"])
-
-
-friendships = [
-    (0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
-    (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)
-]
-
-# for i in friendships:
-#     print("me: {0}".format(i))
-#     print("friends: {0}".format(friendships[:]))
-#     print("--------------------")
-
-
-# friendships = {user["id"]: [] for user in users}
-
-
-def find_friends(i, friendships):
-    # friends = friendships[i]
-
-    for i in friendships:
-        print("friends: {0}".format(i))
-        # print("me : {0}".format(friendships[:]))
-
-    # return friends[i]
-
-
-def find_users(i, users):
-    for i in users:
-        print("users: {0}".format(i))
-    
-    friendships = {user["id"]: [] for user in users}
-
-    for i in friendships:
-        if friendships == "\0":
-            print("i -> {0} -- user[friends] -> {1}".format(i, friendships))
-            return friendships
-        
-        else:
-            print("Not Null")
-            return 0
-    # for i in user["friends"]:
-    #     if user["friends"] == "":
-    #         print("i -> {0} -- user[friends] -> {1}".format(i, user["friends"]))
-    #         return user["friends"]
-
-    #     else:
-    #         print("Not Null")
-    #         return 0
-
-
-def number_of_friends(user):
-    user_id = user["id"]
-    friend_ids = friendships[user_id]
-    return len(friend_ids)
-
+from get_user_data.friendships import *
+from attribute.user_attribute import *
 
 if __name__ == "__main__":
     for i in users:
