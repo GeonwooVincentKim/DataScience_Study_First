e0eb0c35e8542419342d407a0309f552a5533db7
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-11 15:41:45 +0900

    [Feature] - Get the number of users

--- get_user_data/new_friendships.py
+++ get_user_data/new_friendships.py
@@ -7,16 +7,26 @@ from attribute.user_attribute import *
 friendships = {user["id"]: [] for user in users}
 print(friendships)
 
-friendship_example = [
-    (0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
-    (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)
-]
-
-
 for i, j in friendships_pairs:
     friendships[i].append(j)
     friendships[j].append(i)
 
-# for i, j in friendships_pairs:
-    # friendships[i].append(j)
-    # friendships[j].append(i)
+
+def number_of_friends(user):
+    """Check how many friends does `user` have
+
+    Args:
+        user (dictionary): Get the id of dictionary of friendships
+    """
+    user_id = user["id"]
+    
+    friend_ids = friendships[user_id]
+    return len(friend_ids)
+
+total_connections = sum(number_of_friends(user) for user in users)
+print(total_connections)
+
+num_users = len(users)
+average_connections = total_connections / num_users
+print(average_connections)
+
