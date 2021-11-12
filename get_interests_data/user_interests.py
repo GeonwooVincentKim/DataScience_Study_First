3f85c76619a2bb9b9969aae13d222a2eac4db7df
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-12 11:34:31 +0900

    [Feature] - Set the relative path of User-Interests

---
+++ get_interests_data/user_interests.py
@@ -0,0 +1,16 @@
+import os
+import sys
+# sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__name__))))))
+sys.path.insert(0, 'E:/Project/Python/Python_DataScience/Project_01_DataScience')
+
+from collections import defaultdict
+from get_user_data.count_friends import *
+from get_user_data.recommand_user import *
+
+
+def hi():
+    print("hi")
+
+
+if __name__ == "__main__":
+    hi()
