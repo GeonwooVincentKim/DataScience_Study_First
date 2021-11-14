0ca27e07159a16fdacd1b853175ab904852753ad
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-14 21:43:50 +0900

    [Feature] - User features of checking average salaries by vacation

--- attribute/salary_and_tenures.py
+++ attribute/salary_and_tenures.py
@@ -57,3 +57,44 @@ average_salary_by_bucket = {
 }
 
 print("Dictionary of `Average_salary_bucket` -> {0}".format(average_salary_by_bucket))
+
+
+# Contains the users average vacation days of annual-year
+salaries_and_tenures_and_vacations_list = [
+    (83000, 8.7, 5.2), (88000, 8.1, 5),
+    (48000, 0.7, 4.8), (76000, 6, 5.4),
+    (69000, 6.5, 7.5), (76000, 7.5, 3.8),
+    (60000, 2.5, 9.4), (83000, 10, 7.8),
+    (48000, 1.9, 6.8), (63000, 4.2, 9.2)
+]
+
+for i in salaries_and_tenures_and_vacations_list:
+    print(i)
+
+
+def vacation_bucket(vacation):
+    if vacation < 3:
+        return "You have no enough vacation days on your annual-life"
+
+    elif vacation < 5:
+        return "You should have to spend more time with your family and you"
+
+    elif vacation < 8: 
+        return "You doing great job, but remeber not to forget to spend the time with your family"
+
+    else:
+        return "You doing perfectly in your life!!!"
+
+
+salary_by_tenure_and_vacation = defaultdict(list)
+
+for salary, tenure, vacation in salaries_and_tenures_and_vacations_list:
+    average_vacation = vacation_bucket(vacation)
+    salary_by_tenure_and_vacation[salary].append(salary)
+
+average_salary_by_vacation = {
+    vacation_bucket: len(vacation)
+    for vacation_bucket, vacation in salary_by_tenure_and_vacation.items()
+}
+
+print("Dictionary of `Average_salary_by Vacation` -> {0}".format(average_salary_by_vacation))
