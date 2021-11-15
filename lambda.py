dcd11b74914076f6c0ee608a9c5b2ab288eae71e
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-15 10:13:35 +0900

    [Modify] - Testing some function

--- lambda.py
+++ lambda.py
@@ -4,3 +4,13 @@ def apply_to_one(f):
 
 y = apply_to_one(lambda x: x + 4)
 print(y)
+
+tc = int(input())
+
+for i in range(1, tc):
+    outer_tc = int(input())
+    print(outer_tc)
+
+    for j in range(0, outer_tc):
+        inner_tc = map(int, input().split(" "))
+        print(inner_tc)
